113868d68ec488e40c07e8476dd820cbee3709cb
George Cave <cave@hey.com>
2023-04-06 15:37:08 +0100

    Update with new graphics

--- scripts.js
+++ scripts.js
@@ -1,31 +1,40 @@
 'use strict';
 
-const ctx = new AudioContext();
-const o = ctx.createOscillator();
-o.frequency.value = 261.63;
-
 // When page loads
 document.addEventListener("DOMContentLoaded", () => {
 
+	const circle = document.querySelector('.circle')
+
 	// Click buton to connect
-	document.querySelector(".connect").addEventListener("click", async (e) => {
-		e.preventDefault();
-		await serial.connect();
-	});	
-	
-	// Click button to start sinewave
-	document.querySelector('.start').addEventListener('click', (e) => {
-		e.preventDefault();
-		o.start(0);
-		o.connect(ctx.destination);
+	circle.addEventListener("click", async (e) => {
+		e.preventDefault()
+		const connected = await serial.connect()
+		if(connected){
+			circle.classList.add('is-connected')
+			oscillator.start(0)
+		}
+	})
+		
+	const ctx = new AudioContext()
+	const oscillator = ctx.createOscillator()
+	const gain = ctx.createGain()
+
+	oscillator.connect(gain)
+	gain.connect(ctx.destination)
+
+	// Create Serial connection
+	const serial = new Serial({
+
+		// When a new serial value arrives, set everything
+		// Level = 0..100 expected value
+		read_callback: (msg) => {
+			let level = parseInt(msg)
+			gain.gain.value = Math.min(1, (level*10/100))
+			oscillator.frequency.value = 250 + 20*level
+			circle.style.transform = `scale(${1+(level*8/100)})`
+		}
 	});
+
 });
 
-// Create Serial connection
-const serial = new Serial({
 
-	// When a new serial value arrives, set the sinewave frequency based on that value
-	read_callback: (msg) => {
-		o.frequency.value = 250 + 15*parseInt(msg);
-	}
-});
